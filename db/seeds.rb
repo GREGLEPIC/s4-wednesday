ddd614fe0a1df5cc41b23780bfa631ddf2bc0ea7
Greg Rouxel Oldrà <rouxel.g@gmail.com>
2019-01-30 18:27:37 +0100

    my blog DONE

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+10.times do
+  user = User.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email)
+end
+
+10.times do
+  article = Article.create!(title: Faker::Book.title, content: Faker::ChuckNorris.fact, user_id: Faker::Number.between(1, 13))
+end
+  
+15.times do
+  jadore = Like.create!(content: Faker::ChuckNorris.fact, user_id: Faker::Number.between(1, 13), article_id: Faker::Number.between(1, 2))
+end
+  
+10.times do
+  comment = Comment.create!(content: Faker::ChuckNorris.fact, user_id: Faker::Number.between(1, 13), article_id: Faker::Number.between(1, 2))
+end
